ee103e39dd6f2783c8939c8be0822ec6cc3e2e0b
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-25 13:06:45 -0300

    backend: foram criados os controllers da aplicação, OngController, IncidentController, ProfileController, SessionController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+routes.post('/sessions', SessionController.create);
+
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+
 
 module.exports = routes;
 
@@ -17,7 +31,7 @@ module.exports = routes;
 	- Logout de ONG
 	- Cadastro de ONG - ok
 	- Cadastrar novos casos
-  - Deletar casos
+  - Deletar casos - ok
   - Listar ONGs - ok 
 	- Listar casos específicos de uma ONG
 	- Listar todos os casos
