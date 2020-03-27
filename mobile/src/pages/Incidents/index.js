915c33b6ebd9aaa4e62e31641853554ee655fbd9
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-26 22:12:35 -0300

    mobile: criado as pages, criado arquivo src/routes.js utilizando react navigation

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Incidents() {
+  return <Text>Incidents</Text>;
+}
